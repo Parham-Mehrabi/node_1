e3ce3cae74f27f586ce3c0e9d2ea7b09b46ae750
Parham-Mehrabi <parham.mehrabi.webdev@gmail.com>
2023-07-18 16:46:24 +0330

    use privious method to delete the object because we want to get 404 when there is no student with that id

--- data/student_queries.js
+++ data/student_queries.js
@@ -47,8 +47,8 @@ async function updateStudent(id, updated_fields){
 
 }
 async function deleteStudent(id){
-    // const student = await Student.findByIdAndRemove(id)
-    const student = await Student.deleteOne({_id: id})
+    const student = await Student.findByIdAndRemove(id)
+    // const student = await Student.deleteOne({_id: id})
     if (!student) return;
     return student
 }
