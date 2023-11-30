e30d3260f7f9777b63ce69dff1dc932abcd1c9b8
parham <parham.mehrabi.webdev@gmail.com>
2023-11-30 15:51:28 +0330

    add object ID validation module to our validate_student_id

--- object_id/example.js
+++ object_id/example.js
@@ -3,4 +3,6 @@ const mongoose = require('mongoose')
 const id = new mongoose.Types.ObjectId()
 
 
-console.log(id.getTimestamp());
+console.log(id.getTimestamp());     
+
+console.log(mongoose.Types.ObjectId.isValid(id))        // check if the id is valid
--- validations/validate_student_id.js
+++ validations/validate_student_id.js
@@ -1,13 +1,15 @@
 const Joi = require('joi');
+Joi.objectId = require('joi-objectid')(Joi)
 
 
 function validateStudentID(data) {
     const schema = Joi.object().keys({
-        friend_id: Joi.string().regex(/^[0-9a-fA-F]{24}$/).required()
+        // friend_id: Joi.string().regex(/^[0-9a-fA-F]{24}$/).required(),
+        friend_id: Joi.objectId().required(),
     });
 
-    resault = schema.validate(data)
-    return resault
+    result = schema.validate(data)
+    return result
 }
 
 module.exports = validateStudentID;
