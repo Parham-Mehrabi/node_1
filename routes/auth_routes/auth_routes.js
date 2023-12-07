b31105f9151707b45384fa90dee7fc3e9673c2aa
parham <parham.mehrabi.webdev@gmail.com>
2023-12-07 15:43:53 +0330

    check if user already exist

--- routes/auth_routes/auth_routes.js
+++ routes/auth_routes/auth_routes.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const createNewUser = require('../../data/auth_queries/auth_register')
+const AuthUser = require('../../models/authentication/student_model')
 
 const auth_route = express.Router()
 
@@ -7,7 +8,8 @@ auth_route.post('/register', async (req, res) => {
     // validation:
     // const { error } = ourImaginaryRegisterValidateFunction(req.body)
     // if (error) return res.status(400).send(error message)
-
+    let user = await AuthUser.findOne({email: req.body.email});
+    if (user) return res.status(400).send('user already registered')
     try {
 
         const new_student = await createNewUser(req.body)
