67127208fcb2b570a395bffb04693cbcc270fe3b
parham <parham.mehrabi.webdev@gmail.com>
2023-12-13 04:10:52 +0330

    replace our costume wrapper with express-async-errors package (the changes are only in our latest register route)

--- routes/auth_routes/auth_routes.js
+++ routes/auth_routes/auth_routes.js
@@ -7,18 +7,27 @@ const bcrypt = require('bcrypt')
 const jwt = require('jsonwebtoken')
 const config = require('config')
 const login_required = require('../../middlewares/login_required')
-const error_handler = require('../../middlewares/error_handler')
+// const error_handler = require('../../middlewares/error_handler')
 
 const auth_route = express.Router()
 
-auth_route.post('/register', error_handler(async (req, res) => {        // here we wrapped our route-handler to the wrapper
+// auth_route.post('/register', error_handler(async (req, res) => {        // here we wrapped our route-handler to the wrapper
+//     let user = await AuthUser.findOne({ email: req.body.email });
+//     if (user) return res.status(400).send('user already registered')
+//     const raw_password = req.body.password
+//     req.body.password = await hashedPassword(raw_password)
+//     const new_student = await createNewUser(req.body)
+//     return res.send(_.pick(new_student, ['_id', 'email']))
+// }))
+
+auth_route.post('/register', async (req, res) => {        // here we wrapped our route-handler to the wrapper
     let user = await AuthUser.findOne({ email: req.body.email });
     if (user) return res.status(400).send('user already registered')
     const raw_password = req.body.password
     req.body.password = await hashedPassword(raw_password)
     const new_student = await createNewUser(req.body)
     return res.send(_.pick(new_student, ['_id', 'email']))
-}))
+})
 
 
 auth_route.post('/login', async (req, res) => {
