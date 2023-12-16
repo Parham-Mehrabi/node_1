8905b0a4b94ff44d7654d3f5d3c212227bc8fd72
parham <parham.mehrabi.webdev@gmail.com>
2023-12-16 06:04:48 +0330

    using winston execption handle to handle project errors outside request pipeline and kill the proccess

--- index.js
+++ index.js
@@ -23,10 +23,15 @@ winston.add(new winston.transports.MongoDB({
     level: 'error'
 }))
 
-process.on('uncaughtException', (error) => {
-    console.log('unexpected error handled')
-    winston.error('UnExpected Error', {metadata: error})
-})
+// process.on('uncaughtException', (error) => {
+//     console.log('unexpected error handled')
+//     winston.error('UnExpected Error', {metadata: error})
+//     process.exit(1)
+// })
+
+winston.exceptions.handle(new winston.transports.File({filename: 'unexpectedErrors.log'}))
+// this one do the job itself and exit the app and its the correct way to handle it
+// we should restart the project using process managers after such errors happend
 
 throw new Error('Some Error happened during start up.')
 
